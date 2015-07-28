4f8fc92ecfc5f63034c244afdff4c9c803ea3431
majicmoo <megan@theparkes.me.uk>
2015-07-28 09:25:53 +0100

    added project page

--- Website/SkyStore/views/ProductController.py
+++ Website/SkyStore/views/ProductController.py
@@ -6,7 +6,7 @@ from Website.SkyStore.models.Product import Product
 def product_handler(request, product_id):
     p = get_object_or_404(Product, pk=product_id)
     try:
-        product = p.choice_set.get(pk=request.POST['choice'])
+        product = Product.objects.get(pk=product_id)
         return render(request, "product.html", {"product": product})
 
     except (KeyError, Product.DoesNotExist):
