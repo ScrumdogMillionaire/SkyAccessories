a9bcf32304fd9effdd0c26a6eff3c614562414c2
Tom Lloyd <tom.w.lloyd@googlemial.com>
2015-07-29 21:59:36 +0100

    completed info page

--- SkyApp/app/src/main/java/com/bootcamp/skyapp/InfoActivity.java
+++ SkyApp/app/src/main/java/com/bootcamp/skyapp/InfoActivity.java
@@ -1,10 +1,12 @@
 package com.bootcamp.skyapp;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class InfoActivity extends Activity {
@@ -13,6 +15,31 @@ public class InfoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        String fontPath = "skymed.ttf";
+        Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
+
+        TextView title = (TextView) findViewById(R.id.infotitle);
+        title.setTypeface(tf);
+
+        TextView star = (TextView) findViewById(R.id.starText);
+        star.setTypeface(tf);
+
+        TextView loc = (TextView) findViewById(R.id.locationText);
+        loc.setTypeface(tf);
+
+        TextView user = (TextView) findViewById(R.id.userText);
+        user.setTypeface(tf);
+
+        TextView info = (TextView) findViewById(R.id.infoText);
+        info.setTypeface(tf);
+
+        TextView log = (TextView) findViewById(R.id.logoutText);
+        log.setTypeface(tf);
+
+        TextView ver = (TextView) findViewById(R.id.versionText);
+        ver.setTypeface(tf);
+
     }
 
     @Override
